ce26afe9cb34b9ae8f10941c63c110cb4e39af3c
gustavo13101986 <gustalondon2016@gmail.com>
2021-10-11 18:30:51 -0500

    add function to consume api

---
+++ src/utils/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+
+let api = axios.create({
+    headers: {
+        "Client-ID": "pr8b4z1s3813jo11ddtt7dptju3gn7",
+        'Authorization' : 'Bearer uaoyaw0qymj8lx2w828ig6nfddk7zl'
+
+    }
+})
+
+export default api
